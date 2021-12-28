b664878dd7178a4cc668504390502aea93607707
Julian Baumgartner <jbaumgartner93@gmail.com>
2021-12-28 04:46:29 -0500

    fix startup script

--- vm_startup_script.sh
+++ vm_startup_script.sh
@@ -3,7 +3,7 @@
 #   instance-name --metadata-from-file startup-script=idle-shutdown.sh` and reboot
 # NOTE: requires `bc`, eg, sudo apt-get install bc
 # Modified from https://stackoverflow.com/questions/30556920/how-can-i-automatically-kill-idle-gce-instances-based-on-cpu-usage
-threshold=0.1
+threshold=0.05
 
 count=0
 wait_minutes=30
@@ -13,13 +13,18 @@ do
   load=$(uptime | sed -e 's/.*load average: //g' | awk '{ print $1 }') # 1-minute average load
   load="${load//,}" # remove trailing comma
   res=$(echo $load'<'$threshold | bc -l)
-  if (( $res ))
+  if (( $res == 1 ))
   then
     echo "Idling.."
     ((count+=1))
   fi
+  if (($res ==  0)) && (($count > 0 ))
+  then
+     echo "Resetting count"
+    (( count=0 ))
+  fi
   echo "Idle minutes count = $count"
-
+  echo "CPU load = $load"
   if (( count>wait_minutes ))
   then
     echo Shutting down
